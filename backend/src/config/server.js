7e889b8296886068f558072d67f2809d6e269463
bruno <brunodoliveirapereira@gmail.com>
2020-05-19 17:21:28 -0300

    Habilitando Cors e finalizando

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,9 +3,11 @@ const port = 3004
 const express = require('express')
 const bodyParser = require('body-parser')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`Executando na porta ${port}`);
