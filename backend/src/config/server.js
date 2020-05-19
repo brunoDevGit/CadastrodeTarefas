68d8e8120e5754ad7d7102d0e7c323c543d7b28a
bruno <brunodoliveirapereira@gmail.com>
2020-05-19 14:58:43 -0300

    configurando servido conectando com banco mapeando rotas

---
+++ backend/src/config/server.js
@@ -0,0 +1,15 @@
+const port = 3004
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
+
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`Executando na porta ${port}`);
+    
+})
+
+module.exports = server
