9c943be135c8b687bda880f66681b5b85da82714
Bruno de Oliveira <brunodoliveirapereira@gmail.com>
2020-05-24 16:58:10 -0300

    iniciando migracao para redux criando reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,23 @@
+import {combineReducers} from 'redux'
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar',
+            done: true
+        }, {
+            _id:2, 
+            description: 'Reuniao com a equipe',
+            done: false
+        }, {
+            _id: 3,
+            description: 'Consulta medica',
+            done: false
+        
+        }]
+    })
+})
+
+export default rootReducer
